c5349cb198bec9b9064a9ac80c76d60fe07f1558
Harry Metcalfe <harry@dxw.com>
2014-07-16 22:22:34 +0100

    Fixed enqueued jquery to bower version and fixed paths

--- app/assets.php
+++ app/assets.php
@@ -1,19 +1,25 @@
 <?php
 
-add_action('init', function () {
-  remove_action('wp_enqueue_scripts', 'roots_scripts', 100);
-});
-
 add_action('wp_enqueue_scripts', function () {
-  
   /**
-   *  DO NOT ADD THINGS HERE
-   *  Unless they are very commonly used and may also be included by plugins (libraries such as jquery, for example)
-   *  Everything else should be included in the gruntfile or in the js directory so that it is compiled into main.min.js
+   *  Do not add javascript to your theme here, unless you're sure you should.
+   *
+   *  Normally, you should add Javascript to assets/js/main.js or make a file in assets/js/plugins.
+   *
+   *  You can/should enqueue a script here only if it is a widely used library that is required by a plugin (or is likely to be later)
    **/
-  wp_enqueue_script('_jquery',   get_template_directory_uri() . '/build/bower_components/jquery/jquery.min.js' );
-  wp_enqueue_script('modernizr', get_template_directory_uri() . '/assets/js/head/modernizr.min.js'             );
-  wp_enqueue_script('main',      get_template_directory_uri() . '/build/main.min.js', array('_jquery', 'modernizr'), '', true);
 
-  wp_enqueue_style ('main',      get_stylesheet_directory_uri() . '/build/main.min.css');
+  // We need to register our own jQuery, because WP is on jQuery 2.x which breaks support for IE 6-8.
+  // This will not affect admin pages
+  // This will break any plugin that requires a feature/behaviour in jQuery 2.x which is missing/different in jQuery 1.10.x
+  wp_deregister_script('jquery');
+  wp_enqueue_script('jquery',   get_template_directory_uri() . '/assets/lib/jquery.min.js');
+
+  // Because it's awesome
+  wp_enqueue_script('modernizr', get_template_directory_uri() . '/assets/lib/modernizr.min.js');
+
+  // Pretty much everything else should be compiled by Grunt.
+  wp_enqueue_script('main',      get_template_directory_uri() . '/assets/main.min.js', array('jquery', 'modernizr'), '', true);
+
+  wp_enqueue_style ('main',      get_stylesheet_directory_uri() . '/assets/main.min.css');
 });
