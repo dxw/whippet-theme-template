61b553830cf1a5b1bf6a3efe3ee4cb15022fc38d
Harry Metcalfe <harry@dxw.com>
2014-07-16 22:22:44 +0100

    Path fixes

--- app/functions.php
+++ app/functions.php
@@ -1,16 +1,18 @@
 <?php
 
+// TODO: refactor me SO HARD
+
 require(__DIR__ . '/whippet/whippet.php');
 require(__DIR__.'/../vendor/vendor.phar');
 
-include(__DIR__.'/../lib/helpers.php');
-include(__DIR__.'/../lib/feeds.php');
-include(__DIR__.'/../lib/assets.php');
-include(__DIR__.'/../lib/acf.php');
-include(__DIR__.'/../lib/thumbnails.php');
-include(__DIR__.'/../lib/pagination.php');
-include(__DIR__.'/../lib/menus.php');
-include(__DIR__.'/../lib/footnotes.php');
-include(__DIR__.'/../lib/widgets.php');
-include(__DIR__.'/../lib/titles.php');
-include(__DIR__.'/../lib/wrapper.php');
+include(__DIR__.'/lib/helpers.php');
+include(__DIR__.'/lib/feeds.php');
+include(__DIR__.'/lib/assets.php');
+include(__DIR__.'/lib/acf.php');
+include(__DIR__.'/lib/thumbnails.php');
+include(__DIR__.'/lib/pagination.php');
+include(__DIR__.'/lib/menus.php');
+include(__DIR__.'/lib/footnotes.php');
+include(__DIR__.'/lib/widgets.php');
+include(__DIR__.'/lib/titles.php');
+include(__DIR__.'/lib/wrapper.php');
