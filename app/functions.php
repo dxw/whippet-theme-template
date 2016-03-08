a1ea7a890ff0c49eac3efe340ebdf77ac5d72c11
Tom Adams <tom@dxw.com>
2016-03-08 09:52:12 -0500

    Add autoloading
    PSR-4 autoloading allows loading our code without pages of `require`
    statements.

--- app/functions.php
+++ app/functions.php
@@ -2,6 +2,11 @@
 
 require __DIR__ . '/../vendor.phar';
 
+// Autoload
+$loader = new \Aura\Autoload\Loader();
+$loader->register();
+$loader->addPrefix('MyTheme', __DIR__);
+
 /*
  * Libraries and support code
  */
