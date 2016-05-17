38199292a6b707e74f1b16b01051946c6465013c
Tom Adams <tom@dxw.com>
2016-05-17 15:25:36 -0400

    Test Theme/Helpers

--- app/Theme/Helpers.php
+++ app/Theme/Helpers.php
@@ -6,20 +6,24 @@ class Helpers implements \Dxw\Iguana\Registerable
 {
     public function register()
     {
-        add_action('wp_footer', function () {
-            ?>
-            <script type="text/javascript">
-                var _gaq = _gaq || [];
-                _gaq.push(['_setAccount', 'TRACKING_CODE']);
-                _gaq.push(['_trackPageview']);
+        add_action('wp_footer', [$this, 'wpFooter']);
+    }
 
-                (function() {
-                    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-                })();
-            </script>
-            <?php
-        });
+    public function wpFooter()
+    {
+        ?>
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'TRACKING_CODE']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
+        <?php
+
     }
 }
