ca9d6618dda54a491a1a509ed918a8e3db233e36
Tom Adams <tom@dxw.com>
2016-05-18 14:19:32 -0400

    Update dependencies

--- app/di.php
+++ app/di.php
@@ -4,7 +4,7 @@ $registrar->addInstance(\Dxw\Iguana\Theme\Helpers::class, new \Dxw\Iguana\Theme\
 $registrar->addInstance(\Dxw\Iguana\Theme\LayoutRegister::class, new \Dxw\Iguana\Theme\LayoutRegister(
     $registrar->getInstance(\Dxw\Iguana\Theme\Helpers::class)
 ));
-$registrar->addInstance(\Dxw\Iguana\Theme\UseAtom::class, new \Dxw\Iguana\Theme\UseAtom());
+$registrar->addInstance(\Dxw\Iguana\Extras\UseAtom::class, new \Dxw\Iguana\Extras\UseAtom());
 
 // Libraries and support code
 $registrar->addInstance(\Dxw\MyTheme\Lib\Whippet\TemplateTags::class, new \Dxw\MyTheme\Lib\Whippet\TemplateTags(
