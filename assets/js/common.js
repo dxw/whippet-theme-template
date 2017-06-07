3e100f79ccf376cae9b7b0a80bb19d6563c38c0c
Robbie Paul <robbie.paul@dxw.com>
2017-06-07 11:25:25 +0100

    (feature) Toggle header menu now has JavaScript

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,5 +3,15 @@
 jQuery(function ($) {
   'use strict'
 
-  // ...
+  // Toggle mobile menu
+  $('button.navigation-toggle').click(function () {
+    if ($('div.menu-header').hasClass('expanded')) {
+      $('div.expanded').removeClass('expanded').slideUp(250)
+      $(this).removeClass('open')
+    } else {
+      $('div.menu-header').addClass('expanded').slideDown(250)
+      $(this).addClass('open')
+    }
+  })
+
 })
