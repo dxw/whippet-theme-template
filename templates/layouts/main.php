0caf29cb26215836fa1258a58b58e632bb1db6b8
Robbie Paul <robbie.paul@dxw.com>
2017-06-07 11:46:42 +0100

    (chore) Sass and templates renamed for consistency

--- templates/layouts/main.php
+++ templates/layouts/main.php
@@ -12,27 +12,13 @@
 
     <!--[if lt IE 7]><div class="alert"><?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?></div><![endif]-->
 
-    <header class="banner" role="banner">
-        <div class="row">
-            <div class="logo">
-                <a href="/"><h1><?php bloginfo('name'); ?></h1></a>
-            </div>
-        </div>
-        <div class="row">
-            <?php get_template_part('partials/nav'); ?>
-        </div>
-    </header>
+    <?php get_template_part('partials/global-header'); ?>
 
     <main class="main" role="main">
         <?php h()->w_requested_template(); ?>
     </main>
 
-    <footer class="content-info" role="contentinfo">
-        <div class="row">
-            <?php dynamic_sidebar('sidebar-footer'); ?>
-            <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-        </div>
-    </footer>
+    <?php get_template_part('partials/global-footer'); ?>
 
     <?php wp_footer(); ?>
 </body>
